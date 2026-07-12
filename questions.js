0740c640b5b3cff07c455009f869aaacc88c3b8e
radiantfloorings <rituagar1985@gmail.com>
2026-07-13 00:28:05 +0530

    Update questions.js

--- questions.js
+++ questions.js
@@ -1,1 +1,26 @@
+const questions = [
+{
+question:"History is the study of:",
+options:[
+"Future events",
+"Past events related to human societies",
+"Only political events",
+"Only wars"
+],
+answer:1,
+explanation:"History is the study of past events related to human societies."
+},
 
+{
+question:"Numismatics is the study of:",
+options:[
+"Coins",
+"Temples",
+"Paintings",
+"Languages"
+],
+answer:0,
+explanation:"Numismatics means study of coins."
+}
+
+];
